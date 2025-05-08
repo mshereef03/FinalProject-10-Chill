4d63a4c49539976bb4ac1e552c662c8e20380f84
Habiba ELkabbany <helkabbany2@gmail.com>
2025-05-08 22:17:53 +0300

    Started order model

--- order/src/main/java/model/Order.java
+++ order/src/main/java/model/Order.java
@@ -1,4 +1,14 @@
 package model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+
+
 }
