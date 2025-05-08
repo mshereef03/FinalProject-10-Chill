c00c45946047b19b818716d3444477b3a30479ee
Habiba ELkabbany <helkabbany2@gmail.com>
2025-05-08 22:25:18 +0300

    finished order model

--- order/src/main/java/model/Order.java
+++ order/src/main/java/model/Order.java
@@ -9,6 +9,41 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
+    private double price;
+    @OneToOne (cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id", referencedColumnName = "id")
+    private Cart cart;
 
+    public Order() {
+    }
+    public Order(int id, double price, Cart cart) {
+        this.id = id;
+        this.price = price;
+        this.cart = cart;
+    }
+
+    public Order(double price, Cart cart) {
+        this.price = price;
+        this.cart = cart;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public Cart getCart() {
+        return cart;
+    }
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 
 }
