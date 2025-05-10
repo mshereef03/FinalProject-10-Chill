28268fc75f648bfd98d872fc2e285a415f3b3fcf
Habiba ELkabbany <helkabbany2@gmail.com>
2025-05-10 13:07:57 +0300

    Added cache to cart

---
+++ order/src/main/java/com/chill/order/config/RedisCacheConfig.java
@@ -0,0 +1,32 @@
+package com.chill.order.config;
+
+import jakarta.persistence.Cache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // Default cache configuration
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) // Default TTL for all caches
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new Jackson2JsonRedisSerializer<>(Object.class)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultConfig)
+                .build();
+    }
+}
--- order/src/main/java/com/chill/order/service/CartService.java
+++ order/src/main/java/com/chill/order/service/CartService.java
@@ -1,7 +1,11 @@
 package com.chill.order.service;
+
 import com.chill.order.model.Cart;
 import com.chill.order.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,18 +20,22 @@ public class CartService {
     public List<Cart> findAllCarts() {
         return cartRepository.findAll();
     }
+    @CachePut(value = "cart_cache", key = "#cart.id")
     public Cart addCart(Cart cart) {
         return cartRepository.save(cart);
     }
+    @CacheEvict(value = "cart_cache", key = "#cartId")
     public void deleteCart(int cartId) {
         cartRepository.deleteById(cartId);
     }
+
+    @Cacheable(value = "cart_cache", key = "#cartId")
     public Optional<Cart> findCartById(int cartId) {
         return cartRepository.findById(cartId);
     }
 
+    @CachePut(value = "cart_cache", key = "#cart.id")
     public Cart updateCart(Cart cart) {
         return cartRepository.save(cart);
     }
-
 }
