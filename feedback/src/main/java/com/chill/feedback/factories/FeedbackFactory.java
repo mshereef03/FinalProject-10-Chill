205b9e6aa08ab6bec000b2674dd4061be123cb3a
mazen <mazzen.m.s0liman@gmail.com>
2025-05-13 05:39:56 +0300

    Refactoring and Modifications

--- feedback/src/main/java/com/chill/feedback/factories/FeedbackFactory.java
+++ feedback/src/main/java/com/chill/feedback/factories/FeedbackFactory.java
@@ -1,10 +1,4 @@
 package com.chill.feedback.factories;
 
-import com.chill.feedback.models.Feedback;
-import java.util.UUID;
-import java.util.Date;
-
-public abstract class FeedbackFactory {
-    public abstract Feedback createFeedback(Feedback feedback);
-    public abstract boolean supports(Feedback feedback);
+public class FeedbackFactory {
 }
