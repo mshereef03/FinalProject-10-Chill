cd9a04db26d43f41e26cb3abb958bbbe66db9424
Elkott1 <103424889+Elkott1@users.noreply.github.com>
2025-05-13 19:16:35 +0300

    feedback communication using rabbit mq (listen and create feedback to be added to the feedback repository)

--- feedback/src/main/java/com/chill/feedback/service/FeedbackService.java
+++ feedback/src/main/java/com/chill/feedback/service/FeedbackService.java
@@ -1,15 +1,37 @@
-package com.chill.feedback;
+package com.chill.feedback.service;
 
+import com.chill.feedback.dtos.FeedbackDTO;
+import com.chill.feedback.models.Feedback;
 import com.chill.feedback.rabbitmq.RabbitMQConfig;
+import com.chill.feedback.repositories.FeedbackRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FeedbackService {
 
+    @Autowired
+    private FeedbackRepository feedbackRepository;  // Assuming you have a FeedbackRepository for DB operations
+
     @RabbitListener(queues = RabbitMQConfig.FEEDBACK_QUEUE)
-    public void processFeedback(String feedbackId) {
-        System.out.println("Processing feedback with ID: " + feedbackId);
-        // Perform operations like saving feedback to DB, sending notifications, etc.
+    public void receiveFeedbackMessage(FeedbackDTO feedbackDTO) {
+        // Spring will automatically convert the FeedbackDTO from the message
+
+        // Create feedback object and save it to the database
+        Feedback feedback = new Feedback() {
+            @Override
+            public Feedback update(FeedbackDTO feedbackDTO) {
+                return null;
+            }
+        };
+        feedback.setUserId(feedbackDTO.getUserId());
+        feedback.setVendorId(feedbackDTO.getVendorId());
+        feedback.setOrderId(feedbackDTO.getOrderId());
+        feedback.setComment(feedbackDTO.getComment());
+
+        feedbackRepository.save(feedback);  // Save feedback to the database
+
+        System.out.println("Feedback created successfully with ID: " + feedback.getId());
     }
 }
