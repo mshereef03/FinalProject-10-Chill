7cf601cd84da2322a0e3194d89b8981c9b557ba4
mazen <mazzen.m.s0liman@gmail.com>
2025-05-18 16:50:08 +0300

    Remove eureka

--- feedback/src/main/java/com/chill/feedback/FeedbackMicroserviceApplication.java
+++ feedback/src/main/java/com/chill/feedback/FeedbackMicroserviceApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
