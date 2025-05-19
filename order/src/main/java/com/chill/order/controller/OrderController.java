35cf166b1da14dfe4f158d48d669bc3310fecd10
Mohamed Abdelwahab <mshereef2003@gmail.com>
2025-05-19 23:16:23 +0300

    cancelOrder modification

--- order/src/main/java/com/chill/order/controller/OrderController.java
+++ order/src/main/java/com/chill/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.chill.order.controller;
 
 import com.chill.order.model.Cart;
+import com.chill.order.model.MysteryBagDTO;
 import com.chill.order.model.Order;
 import com.chill.order.repository.CartRepository;
+import com.chill.order.service.CartService;
 import com.chill.order.service.CommandPattern.CancelOrderCommand;
 import com.chill.order.service.CommandPattern.Invoker;
 import com.chill.order.service.CommandPattern.PlaceOrderCommand;
@@ -33,6 +35,9 @@ public class OrderController {
 
     @Autowired
     PromoCodeService promoCodeService;
+
+    @Autowired
+    private CartService cartService;
 
 
     @GetMapping("/{orderId}")
@@ -65,7 +70,19 @@ public class OrderController {
 
     @DeleteMapping("/{orderId}")
     public Order cancelOrder(@PathVariable int orderId) {
-        return orderService.cancelOrder(orderId);
+        try
+        {
+            Order order = orderService.cancelOrder(orderId);
+            Cart cart = order.getCart();
+            int cartId = cart.getId();
+            List<MysteryBagDTO> products = cart.getProducts();
+            for (MysteryBagDTO product : products) {
+                cartService.removeMysteryBagFromCart(cartId, product.getId());
+            }
+            return order;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     // discount is percentage
